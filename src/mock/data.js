2e750b0e4d0c9da5b0d774234c71ebb4b6821a11
Cory <Hyakkushiki@users.noreply.github.com>
2021-05-25 01:01:22 +0900

    no XXX

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,9 +2,9 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: 'XXX', // e.g: Welcome to my website
+  title: 'CZ', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
+  description: 'ようこそ！', // e.g: Welcome to my website
 };
 
 // HERO DATA
