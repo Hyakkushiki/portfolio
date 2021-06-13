71e43a97aca6593d815baebf726cff7fa6ad5830
CJ <Hyakkushiki@users.noreply.github.com>
2021-06-14 01:12:26 +0900

    Update data.js

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,9 +2,9 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: 'CZ', // e.g: 'Name | Developer'
-  lang: 'en', // e.g: en, es, fr, jp
-  description: 'ようこそ！', // e.g: Welcome to my website
+  title: 'CZ',
+  lang: 'en',
+  description: 'ようこそ！',
 };
 
 // HERO DATA
@@ -22,7 +22,7 @@ export const aboutData = {
   paragraphOne: "Hello, I'm a mechanical engineer currently located in Japan and I love creating things. I'm an Alumnus of The Georgia Institute of Technology. My degree and working experience has given me a strong background in analysis and design. I love facing challenging problems and being able to design solutions. My hope is that my programming projects can apply my engineering knowledge to real problems.",
   paragraphTwo: '',
   paragraphThree: '',
-  resume: '', // 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  resume: '', // 'https://www.resumemaker.online/es.php' ADD MY RESUME LATER
 };
 
 // PROJECTS DATA
@@ -31,20 +31,11 @@ export const projectsData = [
     id: nanoid(),
     img: 'tic_tac_toe.PNG',
     title: 'tic-tac-toe game',
-    info: 'I created this game in Vue.js. I wanted to make a simple AI for a game.',
+    info: 'I created this game in Vue.js. I wanted to try making a simple "AI" for a game.',
     info2: 'Vue.js',
     url: 'https://hyakkushiki.github.io/tic-tac-toe/',
-    repo: 'https://github.com/Hyakkushiki/tic-tac-toe', // if no repo, the button will not show up
+    repo: 'https://github.com/Hyakkushiki/tic-tac-toe',
   },
-  /* {
-    id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  }, */
 ];
 
 // CONTACT DATA
@@ -57,11 +48,11 @@ export const contactData = {
 // FOOTER DATA
 export const footerData = {
   networks: [
-    // {
-    //   id: nanoid(),
-    //   name: 'twitter',
-    //   url: '',
-    // },
+    {
+      id: nanoid(),
+      name: 'codesandbox',
+      url: 'https://codesandbox.io/u/Hyakkushiki',
+    },
     {
       id: nanoid(),
       name: 'codepen',
@@ -72,15 +63,15 @@ export const footerData = {
       name: 'linkedin',
       url: 'https://www.linkedin.com/in/corwz/',
     },
-    {
-      id: nanoid(),
-      name: 'github',
-      url: 'https://github.com/Hyakkushiki',
-    },
+//     {
+//       id: nanoid(),
+//       name: 'github',
+//       url: 'https://github.com/Hyakkushiki',
+//     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false,
 };
